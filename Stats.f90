04317fdd7193a857e91fd1f06433a6f804debf95
tlind15 <tlind15@gmail.com>
2017-11-09 15:56:04 -0800

    Read in and print array dynamically

--- Stats.f90
+++ Stats.f90
@@ -1,17 +1,21 @@
 Program Stats
 
     character(len=25) :: f
-    print *,"Input name of file to read: "
-    read  (*, *) f 
-    print *,f
-    call READAR
+    real, dimension(:), allocatable :: array
+    integer :: size
+    print *, "Input size of the array: "
+    read *, size
+    allocate(array(size))
+    call READAR(array, size)
+    print *, array
+    deallocate(array)
 End Program Stats
 
-Subroutine READAR
-    integer, dimension(1:10) :: array
-    do i = 1, 10
+Subroutine READAR(array, size)
+    integer, intent(in) :: size
+    real, dimension(1:size) :: array
+    do i = 1, size
         array(i) = i + 1
     end do
-    print *, array
     return
     end
